6cf6d56ddc08ca998feb5633bacf6b8ee56a56d9
AndyRosenberg <andynaterosenberg@gmail.com>
2020-08-27 12:58:56 -0700

    Generic Welcome Mailer

--- jobs/welcome_mailer.rb
+++ jobs/welcome_mailer.rb
@@ -3,14 +3,14 @@ require_relative 'mail_job'
 class WelcomeMailer < MailWorker
   def perform(user)
     message = <<~MSG
-      <h3>Thanks for signing up for Dynaposted!</h3>
+      <h3>Thanks for signing up for #{ENV['APP_NAME']}!</h3>
       <p>Feel free to explore and update your dynamic posts.</p>
     MSG
 
     Mail.deliver do
       to      user['email']
-      from    'Dynaposted <admin@po-it.com>'
-      subject 'Welcome to Dynaposted'
+      from    "#{ENV['APP_NAME']} <admin@po-it.com>"
+      subject "Welcome to #{ENV['APP_NAME']}"
 
       html_part do
         content_type 'text/html; charset=UTF-8'
